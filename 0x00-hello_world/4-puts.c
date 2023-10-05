b7d2ad53717ddbf31798fe4a9ec4aab4036a4e5d
Jannah112 <alaaosman1635@gmail.com>
2023-10-05 11:17:10 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/* this code ptint a statment */
+/**
+ *  this code ptint a statment
+*/
 int main(void)
 {
-
 	puts ( "\"Programming is like building a multilingual puzzle" ); 
 	return 0;
 }
