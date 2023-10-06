4ad4d8770622efaa40eac196dc6326ccca5f2d44
Jannah112 <alaaosman1635@gmail.com>
2023-10-06 13:00:43 -0700

    l

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	chat c;
+
+	for (c = 0 ; c <= 9 ; c++)
+		putchar(c);
+	for (c = 'a' ; a <= 'f' ; c++)
+		putchar (c);
+	putchar('\n');
+	return (0);
+}
