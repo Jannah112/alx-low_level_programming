b8db265a0fff1da6fa3c6059133d24304a6d2780
Jannah112 <alaaosman1635@gmail.com>
2023-10-06 12:34:13 -0700

    l

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0 ; c <= 9 ; c++)
+		ptintf("%d", c);
+	ptintf("\n");
+	return (0);
+}
