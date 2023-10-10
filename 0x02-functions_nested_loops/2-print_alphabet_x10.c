c5a58d747dac5a7aa000988e10f9e0e3ea74d680
Jannah112 <alaaosman1635@gmail.com>
2023-10-10 07:41:33 -0700

    l

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_alphabet_x10- print alphabet in lower case
+ * print_alphabet_x10 - print alphabet in lower case
  */
 void print_alphabet(void)
 {
