3d786e2599a1654a46d118cf03355a131d7bd9f3
Jannah112 <alaaosman1635@gmail.com>
2023-10-10 06:46:38 -0700

    l

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	putchar('_');
+	int l = "p";
+	putchar(l);
+	l = "u";
+	putchar(l);
+	l ="t";
+	putchar(l);
+	l = "c";
+	putchar(l);
+	l = "h";
+	putchar(l);
+	l = "a";
+	putchar(l);
+	l = "r";
+	putchar(l);
+	putchar('\n');
+	return (0);
+}
+
