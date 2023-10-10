008c3deadcdcc12d10c7bea055192a4975e3a3c9
Jannah112 <alaaosman1635@gmail.com>
2023-10-10 11:33:39 -0700

    f

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * int _islower - print if lower
+ * _islower - print if lower
+ * @c - parameter
+ *Return: return 0 or 1
  */
 int _islower(int c)
 {
-	if (c >= 97 && c<= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	else 
 		return (0);
