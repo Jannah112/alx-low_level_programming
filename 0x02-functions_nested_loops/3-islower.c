33ae168d663b61875d8a16463f523e342dada408
Jannah112 <alaaosman1635@gmail.com>
2023-10-10 08:43:30 -0700

    l

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,12 @@
 #include "main.h"
-#include <ctype.h>
 #include <stdio.h>
 /**
  * int _islower - print if lower
  */
 int _islower(int c)
 {
-	return (islower(c));
-}
-void test_islower(int n)
-{
-	int r;
-
-	r = _islower(n);
-	putchar(r + '0');
-	putchar('\n');
+	if (c >= 'a' && c<= 'z')
+		return (1);
+	else 
+		return (0);
 }
