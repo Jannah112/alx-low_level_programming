a91b41b080da4a21dc32aa5e799c52ea16034606
Jannah112 <alaaosman1635@gmail.com>
2023-10-10 14:00:45 -0700

    l

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,19 +7,19 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = 0; a <= 2 ;a++)
+	for (a = 0; a <= 2 ; a++)
 	{
-		for (b = 0; a <=3 ;b++)
+		for (b = 0; a <=3 ; b++)
 		{
-			for (c = 0; a <= 5 ;c++)
+			for (c = 0; a <= 5 ; c++)
 			{
-				for (d = 0; a <= 9 ;d++)
+				for (d = 0; a <= 9 ; d++)
 				{
-					_putchar(a);
-					_putchar(b);
+					_putchar(a + '0');
+					_putchar(b + '0');
 					_putchar(':');
-					_putchar(c);
-					_putchar(d);
+					_putchar(c + '0');
+					_putchar(d + '0');
 				}
 			}
 		}
