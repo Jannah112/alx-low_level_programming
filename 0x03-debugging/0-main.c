28fe04595f75e05cdf435a7c97a6f063a63ed024
Jannah112 <alaaosman1635@gmail.com>
2023-10-11 10:21:27 -0700

    l

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,26 +5,12 @@
 /**
  * main - entry point
  * Return: always 0 (Success)
- * positive_or_negative - func to test
- * @i: parameter
  */
-void positive_or_negative(int i)
+int main(void)
 {
-	srand(time(0));
-	i = rand() - RAND_MAX / 2;
-	if (i > 0)
-		printf("%d is positive\n", i);
-	else if (i == 0)
-		printf("%d is zero\n", i);
-	else
-		printf("%d is negative\n", i);
-}
+	int i;
 
-
-
-static int main(void)
-{
-	positive_or_negative(0);
-	printf("\n";
+	i = 98;
+	positive_or_negative(i);
 	return (0);
 }
