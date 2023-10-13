7d35b13ecb30b6847c7371a5489f7af0ff504097
Jannah112 <alaaosman1635@gmail.com>
2023-10-12 17:25:59 -0700

    f

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - f name
+ */
+void more_numbers(void)
+{
+	int i,j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+			_putchar(j + '0');
+		_putchar('\n');
+	}
+}
