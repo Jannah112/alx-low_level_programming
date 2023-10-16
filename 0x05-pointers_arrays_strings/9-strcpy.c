e176138c14766ed6caf6c7a54ac2fd04025b1c77
Jannah112 <alaaosman1635@gmail.com>
2023-10-16 12:30:26 -0700

    c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,18 +10,16 @@ char *_strcpy(char *dest, char *src)
 {
 	int c = 0;
 	int i;
-	char *f;
 
-	while (*(src + i) != 0)
+	while (*(src + i) !=0)
 	{
 		c = c + 1;
 		i++;
 	}
-	f = (char *) malloc(c * sizeof(char));
 	for (int j = 0; j <= c; j++)
 	{
 		*(dest + j) = *(src + j);
 	}
-	return *dest;
+	return dest;
 }
 	
