d467f3a63efdf255e5b61b05b9ccbf802abdb476
Jannah112 <alaaosman1635@gmail.com>
2023-10-16 09:40:41 -0700

    c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - f name
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int i, n, c;
+
+	while (*(str + i) != 0)
+	{
+		c += 1;
+		i++;
+	}
+	if (c % 2 == 0)
+	{
+		n = c / 2;
+		for (i = n; i != '\0'; i++)
+			_putchar(*(str + i));
+	}
+	else
+	{
+		n = ((c - 1) / 2);
+		for (i = n; i != '\0'; i++)
+			_putchar(*(str + i));
+	}
+	_putchar('\n');
+}}
