da40c8b6347731a846f0cb744a979af1a9095b5b
Jannah112 <alaaosman1635@gmail.com>
2023-10-16 10:59:08 -0700

    c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,11 @@
 void puts2(char *str)
 {
 	int i = 0;
-	int f = 0;
 
 	while (*(str + i) != 0)
 	{
-		_putchar(*(str + f));
-		f = f + 2;
-		i++;
+		_putchar(*(str + i));
+		i = i + 2;
 	}
 	_putchar('\n');
 }
