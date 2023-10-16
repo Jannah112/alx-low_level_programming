6f74904ab4c24d5c05456d8442978f1ea88923ea
Jannah112 <alaaosman1635@gmail.com>
2023-10-16 05:41:59 -0700

    c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,14 @@
 /**
  * _strlen - f name
  * @s: par
+ * Return: return count
  */
 int _strlen(char *s)
 {
-	int count = 1;
+	int count = 0;
 	int i = 0;
 
-	while( *(s + i) != 0)
+	while(*(s + i) != 0)
 	{
 		count += 1;
 		i++;
