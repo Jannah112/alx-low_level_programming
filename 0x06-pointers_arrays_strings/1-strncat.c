dfcb725d6c1954fb96970fd4b6af0f10315ebcfb
Jannah112 <alaaosman1635@gmail.com>
2023-10-19 04:24:32 -0700

    c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - name
+ * @dest: pa
+ * @src: par
+ * @n: pa
+ * Return: return
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j;
+
+	while (dest[i] != 0)
+	{
+		i++;
+	}
+	for(j = i; j < (i + n); j++)
+	{
+		dest[j] = src[j - i];
+	}
+	return (dest);
+}
