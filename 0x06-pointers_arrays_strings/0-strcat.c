2c82c0d9e88d0c2a1100c994a11ced15d7e235ae
Jannah112 <alaaosman1635@gmail.com>
2023-10-19 03:12:45 -0700

    c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcat - name
+ * @dest: par
+ * @src: par
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+	int x, a;
+
+	while (dest[i] != 0)
+	{
+		i++;
+	}
+	while (src[j] != 0)
+	{
+		j++;
+	}
+	x = i + j;
+	for (a = i; a < x; a++)
+	{
+		dest[a] = src[a - i];
+	}
+	dest[x] = '\0';
+	return (dest);
+
