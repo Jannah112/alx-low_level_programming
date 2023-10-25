56b654277fd733dac26cab6158746b3a87b9fbc7
Jannah112 <alaaosman1635@gmail.com>
2023-10-25 12:09:32 -0700

    c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,10 +9,7 @@ int x = 0;
 
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		x++;
-		_strlen_recursion(s + 1);
-	}
-	return (x);
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
