29eb07b7f82d0d01d302f23909c2d1cf318bbbe1
Jannah112 <alaaosman1635@gmail.com>
2023-10-27 08:32:12 -0700

    c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -3,14 +3,4 @@
  * _puts - f name
  * @str: par
  */
-void _puts(char *str)
-{
-	int i = 0;
-
-	while (*(str + i) != 0)
-	{
-		_putchar(*(str + i));
-		i++;
-	}
-	_putchar('\n');
-}
+void _puts(char *str);
