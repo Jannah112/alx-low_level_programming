c3a34f1c59de4207685ee22881989b29037e22d8
Jannah112 <alaaosman1635@gmail.com>
2023-10-28 06:03:09 -0700

    c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/** main - where to start
+ * @argc: number of args
+ * @argv: arg vec
+ * Return: return 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return(0);
+}
