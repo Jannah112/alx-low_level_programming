a23b812c132ca4761afec74e729dc4ab22da9fbf
Jannah112 <alaaosman1635@gmail.com>
2023-10-30 11:45:08 -0700

    c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - f name
+ * @str: gg
+ * Return: gg
+ */
+char *_strdup(char *str)
+{
+	char *x;
+	int len = 0;
+	int i = 0;
+
+	while (str[len] != 0)
+	{
+		len++;
+	}
+	if (str == NULL)
+		return (NULL);
+	x = (char *) (malloc(sizeof(char) * i));
+	if ( x == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+	{
+		x[i] = str[i];
+	}
+	return (x);
+}
