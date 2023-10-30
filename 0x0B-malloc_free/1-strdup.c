b899dde2a7ca6dc476647829c92462a80fe2864a
Jannah112 <alaaosman1635@gmail.com>
2023-10-30 12:03:57 -0700

    c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,12 +16,12 @@ char *_strdup(char *str)
 	{
 		len++;
 	}
-	if (*str == '\0')
+	if (str == NULL)
 		return (NULL);
-	x = (char *) (malloc ((sizeof(char) * len) + 1));
-	if ( x == NULL)
+	x = (char *) (malloc((sizeof(char) * len) + 1));
+	if (x == NULL)
 		return (NULL);
-	for (i = 0; i < len; i++)
+	for (i = 0; str[i] != 0; i++)
 	{
 		x[i] = str[i];
 	}
