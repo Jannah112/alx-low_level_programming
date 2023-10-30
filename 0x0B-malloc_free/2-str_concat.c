7022bf51823706eaec2a239e26cac0729bb90fc3
Jannah112 <alaaosman1635@gmail.com>
2023-10-30 15:14:05 -0700

    l

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - f name
+ * @s1: hh
+ * @s2: cc
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int x = 0, y = 0, a = 0, b = 0;
+	char *s;
+
+	if (s1 != NULL)
+	{
+		while (s1[a] != 0)
+			a++;
+	}
+	if (s2 != NULL)
+	{
+		while (s2[b] != 0)
+			b++;
+	}
+	s = (char *) (malloc(sizeof(char) * (a + b + 1)));
+	if (s == NULL)
+		return (NULL);
+	if (s1 != NULL)
+	{
+		for (x = 0; x < a; x++)
+		{
+			s[x] = s1[x];
+		}
+	}
+	if (s2 != NULL)
+	{
+		for (y = x; y < a + b; y++)
+		{
+			s[y] = s2[y - a];
+		}
+	}
+	s[y] = '\0';
+	return (s);
+}	
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,4 +2,7 @@
 #define MAIN_H
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 #endif
