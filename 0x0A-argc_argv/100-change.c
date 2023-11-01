0b6ea2d79780d0b01782de4819614eea1cccf9b9
Jannah112 <alaaosman1635@gmail.com>
2023-11-01 12:57:32 -0700

    c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,71 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - f name
- * @argc: hh
- * @argv: hh
+ * main - gg
+ * @argc: gg
+ * @argv:
  * Return: hh
  */
 int main(int argc, char *argv[])
 {
-	int x, i,j, a = 1, b = 2, w;
-	int fc[] = {1,2,5,10,25};
-	int **q;
+	int c, nc = 0, i;
+	int a[] = {25, 10, 5, 2, 1};
 
 	if (argc - 1 != 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	x = atoi(argv[1]);
-	if (x <= 0)
+	c = atoi(argv[1]);
+	if (c <= 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	q = (int **) (malloc(sizeof (int *) * 5));
 	for (i = 0; i < 5; i++)
 	{
-		q[i] = (int *) (malloc(sizeof(int) * (x + 1)));
-	}
-	for (i = 0; i < 5; i++)
-	{
-		q[i][0] = fc[i];
-	}
-	for (i = 0; i < 5; i++)
-	{
-		q[i][1] = 0;
-	}
-	for (i = 2; i <= x + 1; i++)
-	{
-		q[0][i] = i - 1;
-	}
-	for (a = 1; a < 5; a++)
-	{
-		for (b = 2; b <= x + 1; b++)
+		if (c >= a[i] && c > 0)
 		{
-			if (x > *q[a])
-			{
-				if (q[0][b] < *q[i])
-					q[a][b] = q[a - 1][b];
-				w = 0;
-				else
-				{
-					q[a][b] = w + 1;
-					w++;
-				}
-				w = 0;
-			}
+			nc += 1;
+			c -= a[i];
 		}
 	}
-	for (i = 0; i < 5; i++)
-	{
-		for (j = 0; j <= x + 1; j++)
-		{
-			printf("%d", q[i][j]);
-
-		}
-		printf("\n");
-	}
+	printf("%d\n", nc);
 	return (0);
 }
