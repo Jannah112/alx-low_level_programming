7800e517d5abf0bc790d71db2aacabdb978b48ab
Jannah112 <alaaosman1635@gmail.com>
2023-11-06 08:03:59 -0800

    c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * print_dog - f bame
  * @d: hh
@@ -7,7 +8,7 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		return (0);
+		return;
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->owner == NULL)
