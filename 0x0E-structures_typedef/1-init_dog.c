8f7ed4ce6051787771ee384e05edf083dccb29a8
Jannah112 <alaaosman1635@gmail.com>
2023-11-06 06:55:45 -0800

    c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - f name
+ * @d: pointer to struct
+ * @name: dog name
+ * @age: age
+ * @owner: gg
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
