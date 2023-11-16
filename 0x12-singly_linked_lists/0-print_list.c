77166527b8b97f6ed19e8f95df2e10f0804c4aae
Jannah112 <alaaosman1635@gmail.com>
2023-11-16 08:57:18 -0800

    c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,20 +9,19 @@
 size_t print_list(const list_t *h)
 {
 	unsigned int count = 0;
-	list_t *t;
-	
-	t = h;
+	const list_t *t = h;
+
 	if (t == NULL)
 		return (0);
 	while (t != NULL)
 	{
 		if (t->str == NULL)
 		{
-			t->str = "(nil)";
-			t->len = 0;
+			printf("[0] (nil)\n");
 		}
+		else
+			printf("[%d] %s\n", t->len, t->str);
 		count++;
-		printf("[%d] %s\n", t->len, t->str);
 		t = t->next;
 	}
 	return (count);
