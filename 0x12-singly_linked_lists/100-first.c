18b1a16d7f9fb7b7fd37db9754c729ea431e3ee7
Jannah112 <alaaosman1635@gmail.com>
2023-11-16 11:12:51 -0800

    c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * first - print text before main
+ */
+void first(void) __attribute__ ((constructor));
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
