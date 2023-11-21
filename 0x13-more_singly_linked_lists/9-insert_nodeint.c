78331b37c3586bff98d436562af261ddf0abb696
Jannah112 <alaaosman1635@gmail.com>
2023-11-21 04:15:53 -0800

    c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - ejdidhhd
+ * @idx: rkdjdj
+ * @n: hdjdjdi
+ * @head: djdjjd
+ * Return: dkdjdhdhj
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int c = 0, i = 0;
+	listint_t *x;
+	listint_t *t = *head;
+
+	while (t != NULL)
+	{
+		c++;
+		t = t->next;
+	}
+	t = *head;
+	if (idx > c + 1)
+		return (NULL);
+	x = malloc(sizeof(listint_t));
+	if (x == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		x->n = n;
+		x->next = NULL;
+		*head = x;
+		return (x);
+	}
+	while (i != idx - 1)
+	{
+		t = t->next;
+		i++;
+	}
+	x->n = n;
+	x->next = t->next;
+	t->next = x;
+	return (x);
+}
