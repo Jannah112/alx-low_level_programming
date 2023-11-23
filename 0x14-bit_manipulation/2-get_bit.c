1cbc3deee69027d2a749e6aa8233ae5fa4345b69
Jannah112 <alaaosman1635@gmail.com>
2023-11-23 09:04:19 -0800

    c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,41 +7,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int x;
-	unsigned int i = 0;
-	char *a;
-	int s = 0;
-
-	if (n == 0)
-	{
-		if (index == 0)
-			return (0);
-		else
-			return (-1);
-	}
-	for (x = n; x > 0; x >>= 1)
-	{
-		s++;
-	}
-	a = (char *) malloc(sizeof(char) * s);
-	if (a == NULL)
+	int x;
+	
+	if (index > 64)
 		return (-1);
-	a[s] = '\0';
-	s--;
-	while (s >= 0)
-	{
-		if ((n >> s) & 1)
-			a[s] = '1';
-		else
-			a[s] = '0';
-		s--;
-	}
-	if (index > strlen(a) - 1)
-		return (-1);
-	for (i = 0; i < strlen(a) ; i++)
-	{
-		if (i == index)
-			break;
-	}
-	return (a[i] - '0');
+	x = (n >> index) & 1;
+	return (x);
 }
