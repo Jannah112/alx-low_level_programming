a53c44f551e243ff6976ab1a1fe84b9a418bc4d8
Jannah112 <alaaosman1635@gmail.com>
2023-11-27 05:45:26 -0800

    c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,12 +7,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int x, y;
+	int x, y, i;
+	int len = 0;
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+		{
+			len++;
+		}
+	}
 	x = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	y = write(x, text_content, strlen(text_content));
+	y = write(x, text_content, len);
 	if (x == -1 || y == -1)
 		return (-1);
 	close(x);
