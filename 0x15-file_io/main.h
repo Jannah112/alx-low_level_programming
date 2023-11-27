62bcda4b77db613d17d48c114b15d98993a6d683
Jannah112 <alaaosman1635@gmail.com>
2023-11-27 04:42:30 -0800

    c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
 ssize_t read_textfile(const char *filename, size_t letters);
