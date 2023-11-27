abd2ae88d44f379ac687b1e3305257938a978275
Jannah112 <alaaosman1635@gmail.com>
2023-11-27 09:04:42 -0800

    c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * main - djdjdjdjd
+ * @argc: hdjdjdjd
+ * @argv: jdjdjdj
+ * Return: djdjddj
+ */
+int main(int argc, char *argv[])
+{
+	int opf, opt, rf, wt, e, er;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	opf = open(argv[1], O_RDONLY);
+	rf = read(opf, buf, 1024);
+	opt = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	do {
+		if (opf == -1 || rf == -1)
+		{
+		dprintf(STDOUT_FILENO, "Error: Can't read from file%s\n", argv[1]);
+		exit(98);
+		}
+		wt = write(opt, buf, rf);
+		if (wt == -1 || opt == -1)
+		{
+		dprintf(STDOUT_FILENO, "Error: Can't write to%s\n", argv[2]);
+		exit(99);
+		}
+		rf = read(opf, buf, 1024);
+		opt = open(argv[2], O_WRONLY | O_APPEND);
+	} while (rf > 0);
+	e = close(opf);
+	if (e == -1)
+		dprintf(STDOUT_FILENO, "Error: Can't close fd%d\n", opf);
+	er = close(opt);
+	if (er == -1)
+		dprintf(STDOUT_FILENO, "Error: Can't close fd%d\n", opt);
+	return (0);
+}
+
