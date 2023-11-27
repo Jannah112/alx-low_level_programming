6cd46dd3db3561150349db697ceb4c6b9f40c8bc
Jannah112 <alaaosman1635@gmail.com>
2023-11-27 04:29:27 -0800

    c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * read_textfile - djdhdhhddj
+ * @filename: hdjdjdjdj
+ * @letters: riririri
+ * Return: djdjdj
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *s;
+	ssize_t x, y, z;
+
+	s = malloc(sizeof(char) * letters);
+	if (s == NULL)
+		return (0);
+	x = open(filename, O_RDONLY);
+	if (x == -1)
+	{
+		free(s);
+		return (0);
+	}
+	y = read(x, s, letters);
+	if (y == -1)
+	{
+		free(s);
+		return (0);
+	}
+	z = write(1, s, y);
+	if (z == -1)
+	{
+		free(s);
+		return (0);
+	}
+	free(s);
+	return (z);
+}
