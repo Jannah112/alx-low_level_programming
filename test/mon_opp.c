d109f46763f70172008b1a7348b64e07f33f88d0
Jannah112 <139776503+Jannah112@users.noreply.github.com>
2024-01-18 15:55:57 +0200

    Create mon_opp.c

---
+++ test/mon_opp.c
@@ -0,0 +1,26 @@
+/**
+ * mod - dhfjdj
+ * @stack: fjfjfj
+ * @line_number: fjfjfj
+ */
+void mod(stack_t **stack, unsigned int line_number)
+{
+	int num;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+	fprintf(stderr, "L<%d>: can't add, stack too short\n", line_number);
+	exit (EXIT_FAILURE);
+	}
+  if ((*stack)->n == 0)
+  {
+    fprintf(stderr, "L<%d>: division by zero\n", line_number);
+    exit (EXIT_FAILURE);
+  }
+	*stack = (*stack)->next;
+	sum = (*stack)->n + (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+/**
