10b920144acf44afea5fcf365b5fd66b6e9ac12f
Jannah112 <139776503+Jannah112@users.noreply.github.com>
2024-01-18 14:49:38 +0200

    Update mon_p.c

--- test/mon_p.c
+++ test/mon_p.c
@@ -14,3 +14,33 @@ void pop(stack_t **stack, unsigned int line_number)
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
+/**
+* push_top - djd
+* @value: djdjdj
+*/
+void push_top(char *value){
+struct_t *new;
+if (value == NULL){}
+if (value[0] == '-')
+{
+value++;
+x = -1;}
+for (i = 0; value[i] != 0; i++)
+{ if(isdigit(value[i])) err}
+ac_val = atoi(value) * x;
+new = malloc(sizeof(struct_t));
+if (new == NULL){Err}
+new->prev = NULL;
+new->n = ac_val;
+if (head == NULL)
+{ new->next = NULL;
+   head = new;
+}
+else:
+{
+   while (head->prev != NULL)
+         head = head->prev;
+    new->next = head;
+    head->prev = new;
+    head = new;
+}
