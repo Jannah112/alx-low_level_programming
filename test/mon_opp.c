79b6b61202346b816d650913c7091c30bd3a2985
Jannah112 <alaaosman1635@gmail.com>
2024-01-18 06:33:37 -0800

    c

--- test/mon_opp.c
+++ test/mon_opp.c
@@ -41,3 +41,33 @@ void swap(stack_t **stack, unsigned int line_number)
 	(*stack)->n = (*stack)->next->n;
 	(*stack)->next->n = temp;
 }
+/**
+ * pint - djfjfj
+ * @stack: fjfjfj
+ * @line_number: djrjfj
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL)
+	{
+	fprintf(stderr, "L<%d>: can't pint, stack empty\n", line_number);
+	exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
+/**
+ * pall - rjrjrj
+ * @stack: fjrjf
+ * @line_number: fjfdj
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL && (*stack) == NULL)
+	{
+		while ((*stack)->next != NULL)
+		{
+			printf("%d\n", (*stack)->n);
+			*stack = (*stack)->next;
+		}
+	}
+}
