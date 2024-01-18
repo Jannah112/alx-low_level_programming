2889bfbe2cf27905404e1e6f630096101faaf96f
Jannah112 <139776503+Jannah112@users.noreply.github.com>
2024-01-18 17:03:09 +0200

    Update mon_exe.c

--- test/mon_exe.c
+++ test/mon_exe.c
@@ -6,15 +6,17 @@
 */
 void exe_token(char *token, char *value, unsigned int ln)
 {
-  void (*t_func)(struct,int);
-          if (token == "push")
-          { push_top(value);}
-           else:{
-          t_func = get_func(token);
+          void (*t_func)(struct,int);
+          if (token[0] == '#')
+                return;
+          if (strcmp(token, "push") == 0)
+               push_top(value, ln);
+          else
+            t_func = get_func(token);
           if (t_func == NULL)
           {
-                fprintf(stderr, "L<%d>: unknown instruction <%s>\n", ln, token);
-                exit (EXIT_FAILURE);
+          fprintf(stderr, "L<%d>: unknown instruction <%s>\n", ln, token);
+          exit (EXIT_FAILURE);
            }
           t_func(&head, ln);}
 }
