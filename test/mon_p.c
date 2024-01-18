e8968dd4e544296c82dc8a190ddc570c8cb38f8a
Jannah112 <139776503+Jannah112@users.noreply.github.com>
2024-01-18 14:24:12 +0200

    Create mon_p.c

---
+++ test/mon_p.c
@@ -0,0 +1,10 @@
+/**
+* pop - hfhfj
+* @stack: djdjd
+* @line_number: djdjdjdj
+*/
+void pop(stack_t **stack, unsigned int line_number)
+{
+  if (stak == NULL || *stack = NULL)
+  {
+       fprintf(stderr, 
