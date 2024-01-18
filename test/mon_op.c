7f5ace79c6c26cc49e8133a63cd3d4fb14503fce
Jannah112 <alaaosman1635@gmail.com>
2024-01-18 06:19:17 -0800

    c

--- test/mon_op.c
+++ test/mon_op.c
@@ -10,7 +10,7 @@ void add(stack_t **stack, unsigned int line_number)
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 	fprintf(stderr, "L<%d>: can't add, stack too short\n", line_number);
-	exit (EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 	}
 	*stack = (*stack)->next;
 	sum = (*stack)->n + (*stack)->prev->n;
@@ -30,7 +30,7 @@ void sub(stack_t **stack, unsigned int line_number)
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 	fprintf(stderr, "L<%d>: can't sub, stack too short\n", line_number);
-	exit (EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 	}
 	*stack = (*stack)->next;
 	num = (*stack)->n - (*stack)->prev->n;
@@ -50,7 +50,7 @@ void mul(stack_t **stack, unsigned int line_number)
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 	fprintf(stderr, "L<%d>: can't mul, stack too short\n", line_number);
-	exit (EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 	}
 	*stack = (*stack)->next;
 	num = (*stack)->n * (*stack)->prev->n;
@@ -70,12 +70,12 @@ void div(stack_t **stack, unsigned int line_number)
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 	fprintf(stderr, "L<%d>: can't div, stack too short\n", line_number);
-	exit (EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 	}
 	if (*stack->n == 0)
 	{
 		fprintf(stderr, "L<%d>: division by zero\n", line_number);
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 	*stack = (*stack)->next;
 	num = (*stack)->n / (*stack)->prev->n;
--- test/mon_opp.c
+++ test/mon_opp.c
@@ -10,13 +10,13 @@ void mod(stack_t **stack, unsigned int line_number)
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 	fprintf(stderr, "L<%d>: can't add, stack too short\n", line_number);
-	exit (EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 	}
-  if ((*stack)->n == 0)
-  {
-    fprintf(stderr, "L<%d>: division by zero\n", line_number);
-    exit (EXIT_FAILURE);
-  }
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L<%d>: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	*stack = (*stack)->next;
 	sum = (*stack)->n + (*stack)->prev->n;
 	(*stack)->n = sum;
@@ -24,3 +24,20 @@ void mod(stack_t **stack, unsigned int line_number)
 	(*stack)->prev = NULL;
 }
 /**
+ * swap - fjfjfi
+ * @stack: jdjdj
+ * @line_number: rirur
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int temp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+	fprintf(stderr, "L<%d>: can't swap, stack too short\n", line_number);
+	exit(EXIT_FAILURE);
+	}
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
