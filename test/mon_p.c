48416993e63d5668c86295d62d81eef119d2cea8
Jannah112 <alaaosman1635@gmail.com>
2024-01-18 04:46:59 -0800

    c

--- test/mon_p.c
+++ test/mon_p.c
@@ -5,6 +5,12 @@
 */
 void pop(stack_t **stack, unsigned int line_number)
 {
-  if (stak == NULL || *stack = NULL)
-  {
-       fprintf(stderr, 
+	if (stak == NULL || *stack = NULL)
+	{
+		fprintf(stderr, "L<%d>: can't pop an empty stack\n", line_number);
+		exit (EXIT_FAILURE);
+	}
+	*stack = (*stack)->nex;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
